9b4632389525e8319691fd562cf6788230aeb9a1
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-09 02:31:06 +0530

    Create kadane_algorithm.cpp

---
+++ kadane_algorithm.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int kadane_algorithm(int ar[],int n){
+
+	int maxSum=INT_MIN;
+	int currSum=0;
+
+	for(int i=0;i<n;i++){
+
+		currSum+=ar[i];
+
+		if(currSum>maxSum){
+			maxSum=currSum;
+		}
+
+		if(currSum<0){
+			currSum=0;
+		}
+	}
+
+	return maxSum;
+}
+
+int main(){
+
+	int n;
+	cin>>n;
+
+	int ar[n];
+	for(int i=0;i<n;i++){
+		cin>>ar[i];
+	}
+
+
+	cout<<"Largest Sum Contiguous Subarray: "<<kadane_algorithm(ar,n)<<endl;
+
+	
+
+
+}
