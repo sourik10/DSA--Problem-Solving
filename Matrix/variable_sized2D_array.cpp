f3ff76ef7e2cdf98aa7eb70deca81f6fa1223676
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-12 10:29:53 +0530

    Create variable_sized2D_array.cpp

---
+++ Matrix/variable_sized2D_array.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*void print(int ar[3][2]){
+	for(int i=0;i<3;i++){
+		for(int j=0;j<2;j++){
+			cout<<ar[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+*/
+
+
+int main(){
+
+	int m=3,n=2;
+
+	int ar[m][n];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<2;j++){
+			cin>>ar[i][j];
+		}
+	}
+
+	for(int i=0;i<3;i++){
+		for(int j=0;j<2;j++){
+			cout<<ar[i][j]<<" ";
+		}
+		cout<<endl;
+		
+	}
+
+
+
+	
+
+}
