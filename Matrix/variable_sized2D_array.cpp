58ad78d5b7a3038c749ae2bda2f0eef75ab8ffaa
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-09-12 10:53:27 +0530

    Update variable_sized2D_array.cpp

--- Matrix/variable_sized2D_array.cpp
+++ Matrix/variable_sized2D_array.cpp
@@ -36,3 +36,38 @@ int main(){
 	
 
 }
+
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int ar[][2],int m){
+	for(int i=0;i<m;i++){
+		for(int j=0;j<2;j++){
+			cout<<ar[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
+
+
+
+int main(){
+
+
+	int ar[3][2]=  { {10, 20},
+					 {30, 40},
+					 {50, 60}};
+				
+
+
+	print(ar,3);
+
+
+
+	
+
+}
+*/
+
