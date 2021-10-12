761f192fb3b5b8022e7845b00795cf6c8462dd92
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-10-12 20:10:00 +0530

    Update countNodes.cpp

--- LinkedList/countNodes.cpp
+++ LinkedList/countNodes.cpp
@@ -4,14 +4,14 @@ class Solution
     //Function to count nodes of a linked list.
     int getCount(struct Node* head){
         if(head==NULL) return 0;
-        if(head->next==NULL) return 1;
-        
-    Node*temp=head; int c=0;
-    while(temp!=NULL){
-        temp=temp->next;
-        c++;
-    }
-    return c;
+        Node* temp=head;
+        int count=0;
+        while(temp!=NULL){
+            temp=temp->next;
+            count++;
+        }
+        return count;
     
     }
 };
+   
