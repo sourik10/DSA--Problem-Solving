4912aca753b09d8641f9d619d7ca303ff64f1568
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-10-12 20:14:16 +0530

    Update insert_BeginEnd.cpp

--- LinkedList/insert_BeginEnd.cpp
+++ LinkedList/insert_BeginEnd.cpp
@@ -2,23 +2,25 @@ class Solution{
   public:
     //Function to insert a node at the beginning of the linked list.
     Node *insertAtBegining(Node *head, int x) {
-
-    Node* temp=new Node(x);
-    temp->next=head;
-    return temp;
-	
-}
-
-    
- //Function to insert a node at the end of the linked list.
-    Node *insertAtEnd(Node *head, int x)  {
-       // Your code here
        Node* t=new Node(x);
-	if(head==NULL) return t; //no node present
-	Node* curr=head;
-	while(curr->next!=NULL){
-	    curr=curr->next;
-	}
-	    curr->next=t;
-	    return head; }
+       t->next=head;
+       head=t;
+       
+       return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       Node* t=new Node(x);
+       if(head==NULL) return t;
+       Node* curr=head;
+       while(curr->next!=NULL){
+           curr=curr->next;
+       }
+       curr->next=t;
+       
+       
+       return head;
+    }
 };
