4e2593305375ea2d962279914d379122ee9a377b
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-11-15 11:50:54 +0530

    Update priorityQueue.cpp

--- Heap/priorityQueue.cpp
+++ Heap/priorityQueue.cpp
@@ -5,8 +5,10 @@ using namespace std;
 int main(){
     priority_queue <int> pq;  //maxHeap (byDefault)
     /*
+    
     minHeap in STL
     priority_queue <int,vector<int>,greater<int>> pq; 
+    
     */
     pq.push(10);
     pq.push(15);
