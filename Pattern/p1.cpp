aa7e4a347734ff6b64c01d109dc472fa5889944a
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-14 20:16:14 +0530

    Update p1.cpp

--- Pattern/p1.cpp
+++ Pattern/p1.cpp
@@ -1,3 +1,4 @@
+//p1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -14,3 +15,104 @@ int main() {
         i++;
     }
 }
+
+
+//p2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+//p3
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<n-j+1;  //reverseNo
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+//p4
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n; 
+    int k=1;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<k++<<" ";  
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+
+//p5
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n; 
+   // int k=1;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<"*";  
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+
+//p6
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n; 
+    int k=1;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<k++<<" ";  
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+
+
+//p7
+
+
