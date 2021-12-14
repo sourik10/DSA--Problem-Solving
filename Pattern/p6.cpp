fee6cb233f1674f07d586428b2906f625a58e2dc
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-14 20:08:43 +0530

    Create p6.cpp

---
+++ Pattern/p6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin>>n; 
+    int k=1;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=i){
+            cout<<k++<<" ";  
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
