9ef59a0f53d1ac9b61c46df04f0d35e62482038c
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-16 00:01:21 +0530

    Update count_setbits.cpp

--- Bit Manipulation/count_setbits.cpp
+++ Bit Manipulation/count_setbits.cpp
@@ -5,10 +5,21 @@ unsigned int countSetBits(unsigned int n)
 { 
 	unsigned int count = 0; 
 	while (n != 0) {
-	    if (n % 2 != 0)
+	    if (n%1)
 	       count++;
 	    n = n>>1;      
 	} 
 	return count; 
 } 
+
+
+int countOnes(int n){
+	int count=0;
+	while(n!=0){
+		n=n&(n-1);
+		count++;
+	}
+	return count;
+}
+
 
