ad603dfeb597a91c2f372f20d71bebbbb636f821
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-18 00:26:25 +0530

    Update swapAlternateElements.cpp

--- Array/swapAlternateElements.cpp
+++ Array/swapAlternateElements.cpp
@@ -1,26 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  int n; cin>>n;
-  int ar[n];
-  for(int i=0;i<n;i++){
-    cin>>ar[i];
-}
-int i=0;
-while(i<n-1){
+void swapAlternatives(int arr[],int n){
   
+  int i=0;
+  while(i<n-1){
+    swap(arr[i],arr[i+1]);
+    i+=2;
     /* 
       int t=ar[i];
       ar[i]=ar[i+1];
       ar[i+1]=ar[i];
      */
-    swap(ar[i],ar[i+1]);
-    i+=2;
+  }
+  //printing array elements 
+  for(int i=0;i<arr.size();i++){
+    cout<<arr[i]<<" ";
+  }
 }
-for(int i=0;i<n;i++){
-    cout<<ar[i]<<" ";
-}
-}
-
-    
