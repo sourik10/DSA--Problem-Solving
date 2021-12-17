4c3b17bb9be7031349d4277e976fd06a461de870
sourik10 <73734325+sourik10@users.noreply.github.com>
2021-12-18 02:18:46 +0530

    Update Intersection.cpp

--- Array/Intersection.cpp
+++ Array/Intersection.cpp
@@ -34,3 +34,38 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
     }
     return res;
 }
+
+//using set in stl
+class Solution {
+public:
+    
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int>s(nums1.begin(),nums1.end());
+        vector<int>v;
+        for(auto x:nums2){
+            if(s.erase(x)){
+                v.push_back(x);
+            }
+        }
+        return v;
+        /*
+        vector<int>res;
+        int i=0,j=0;
+        while(i<nums1.size() and j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+                nums1[i]=INT_MIN;
+                nums1[j]=INT_MIN;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else j++;
+        }
+        return res;
+        */
+    
+    }
+};
