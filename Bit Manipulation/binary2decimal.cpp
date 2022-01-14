d780f6b3c547425ebe5879090a22c1ed0c71a0ac
Sourik Pan <73734325+sourik10@users.noreply.github.com>
2022-01-14 19:17:51 +0530

    Update binary2decimal.cpp

--- Bit Manipulation/binary2decimal.cpp
+++ Bit Manipulation/binary2decimal.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+long long int binary2decimal(int n){
+    int i=0;
+    long long int ans=0;
+
+    while(n!=0){
+        int last=n%10;
+        if(last){
+            ans+=pow(2,i);
+        }
+        n/=10;
+        i++;
+    }return ans;
+}
+
 int main(){
    int n; cin>>n;
    int i=0,ans=0;
